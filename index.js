67dcc0dfb0a7fe6b9a7598fc43b2cd48867c4eb2
LimitToInfinity <21stLimitless@gmail.com>
2021-02-09 22:46:29 -0700

    Adds rooms functionality to keep track of users in rooms.

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const cors = { origin: "*", credentials: true };
 const io = require('socket.io')(http, { cors });
 const port = process.env.PORT || 9000;
 
+const users = {};
+const rooms = {};
+
 io.on('connection', socket => {
   console.log(`a user connected at ${socket.id}`);
+  users[socket.id] = socket.handshake.query.name;
 
   socket.on('private message', (anotherSocketId, message) => {
     socket.to(anotherSocketId).emit('private message', socket.id, message);
   });
 
   socket.on('disconnect', () => {
+    leaveAllRooms(socket.handshake.query.name);
+
     console.log('a user disconnected');
   });
 
@@ -20,15 +26,40 @@ io.on('connection', socket => {
     socket.broadcast.emit('message', message);
   });
 
-  socket.on('join room', room => socket.join(room));
+  socket.on('join room', (room, sendPeople) => {
+    const { name } = socket.handshake.query;
+    socket.join(room)
+    rooms[room]
+      ? rooms[room].push(name)
+      : rooms[room] = [name];
+    sendPeople(rooms[room].filter(person => person !== name));
+  });
   socket.on('leave room', room => {
     socket.leave(room)
-    console.log(socket.rooms);
+    leaveRoom(room, socket.handshake.query.name);
   });
 
   socket.on('room message', (room, message) => {
     socket.to(room).emit('room message', message);
   })
 });
+
+function leaveRoom(room, username) {
+  if (rooms[room]) {
+    const index = rooms[room].indexOf(username);
+    if (index > -1) {
+      rooms[room].splice(index, 1);
+    }
+  }
+}
+
+function leaveAllRooms(username) {
+  Object.keys(rooms).forEach(room => {
+    const foundPerson = rooms[room].find(person => person === username);
+    if (foundPerson) {
+      leaveRoom(room, username);
+    }
+  });
+}
 
 http.listen(port, () => console.log(`listening on port ${port}`));
